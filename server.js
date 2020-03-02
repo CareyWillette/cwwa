a1bf57762fcb66ebd22839e2d2504250a6df9c13
careywillette <carey@bext360.com>
2020-03-02 03:18:38 -0700

    point response to output path

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ const app = express();
 //     }
 // }));
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/dist`));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'))
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/index.html`))
 })
 
 console.log('%c Console listening on port: ' + location.pathname + '://' + process.env.PORT ? process.env.PORT : '8080' + ': ', 'background: #41ff6b; color: #ff4700;');
