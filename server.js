eb2b988069cdbf571af52cce3f94ed5608c8fe47
careywillette <carey@bext360.com>
2020-03-03 02:32:54 -0700

    scrap all excess code. just using skeleton express

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/cwws/index.html'))
 })
 
-console.log('%c Console listening on port: ' + location.pathname + '://' + process.env.PORT ? process.env.PORT : '8080' + ': ', 'background: #41ff6b; color: #ff4700;');
+// console.log('%c Console listening on port: ' + location.pathname + '://' + process.env.PORT ? process.env.PORT : '8080' + ': ', 'background: #41ff6b; color: #ff4700;');
 
 app.listen(process.env.PORT || 8080)
 
